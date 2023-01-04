457ea2e4a99d29028c284b5e02ef779ce9fd64e2
SebiPochi <sebipochi@gmail.com>
2023-01-04 20:27:52 -0300

    whitelist + cors working

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express')
 const routerApi = require('./routes')
+const cors = require('cors')
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/errorHandler')
 
 const app = express()
 const port = 3000
 
 app.use(express.json())
+
+const whiteList = [
+    'http://localhost:8000',
+    'http://localhost:80',
+    'http://localhost:3000',
+]
+const options = {
+    origin: (origin, callback) => {
+        if(whiteList.includes(origin)){
+            callback(null, true)
+        } else {
+            callback(new Error('Not Allowed'))
+        }
+    }
+}
+app.use(cors())
 
 app.get('/', (req, res) => {
 
