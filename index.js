bf5ff7ea68282f7825b9223e7e7eaa7fd51f5389
SebiPochi <sebipochi@gmail.com>
2023-01-04 21:15:55 -0300

    cors whitelist error resolved

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const whiteList = [
     'http://localhost:8000',
     'http://localhost:80',
     'http://localhost:3000',
-    'https://learning-express.onrender.com/'
+    'https://learning-express.onrender.com'
 ]
 const options = {
     origin: (origin, callback) => {
